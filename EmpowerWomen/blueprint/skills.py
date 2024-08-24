506d1f53886bdf9ba1bbfd7668795e37233835c2
YuxiangZou0027 <yzou0027@student.monash.edu>
2024-08-24 20:45:41 +1000

    update requirements.txt

--- EmpowerWomen/blueprint/skills.py
+++ EmpowerWomen/blueprint/skills.py
@@ -1,7 +1,10 @@
 from flask import Blueprint,render_template
-
+from wordcloud import WordCloud
+import io
+import base64
 skills=Blueprint('skills',__name__)
 
 @skills.route('/skills')
 def skills_page():
     return render_template("SkillsPage.html")
+
