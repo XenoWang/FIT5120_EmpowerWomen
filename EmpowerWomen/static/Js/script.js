081249aab26da488e6737d8d87efa526b64accb3
XenoWang <xeno.wanglinhao@gmail.com>
2024-08-31 17:04:35 +1000

    Deleted the javascript for the scroll snapping

--- EmpowerWomen/static/Js/script.js
+++ EmpowerWomen/static/Js/script.js
@@ -5,12 +5,3 @@
     Email: lwan0191@student.monash.edu, yzou0027@student.monash.edu, jsoo0027@student.monash.edu
     Date: 25/08/2024
 */
-
-window.addEventListener('scroll', function () {
-    var navbar = document.getElementById('navbar');
-    if (window.scrollY > 50) {
-        navbar.classList.add('navbar-scrolled');
-    } else {
-        navbar.classList.remove('navbar-scrolled');
-    }
-});
